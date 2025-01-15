c04a60c8635cb1dd38233c38724f1ffdeafe76e0
h-schroeder-coder <hankorschroeder@gmail.com>
2025-01-15 13:14:54 -0600

    removed unnecessary items

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,37 @@
-import React, { useState } from "react";  
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-   
-    function search(event) {
-       event.preventDefault();
-       alert(`Searching for the definition of ${keyword}`);
-   }
+  let [keyword, setKeyword] = useState("");
 
-   function handleKeywordChange(event) {
-       setKeyword(event.target.value);
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
-   }
+  function search(event) {
+    event.preventDefault();
 
-    return (
-      <div className="Dictionary">
-        <form onSubmit={search}>
-          <input
-            type="search"
-            placeholder="Enter a word..."
-            onChange={handleKeywordChange}
-            autoFocus
-          />
-          <input type="submit" value="Search" />
-        </form>
-      </div>
-    );
+    let apiKey = "dfc9t54e5b10fea0dcae14f3826ob4e6";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Enter a word..."
+          onChange={handleKeywordChange}
+          autoFocus
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
 }
