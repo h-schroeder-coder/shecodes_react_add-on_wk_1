b5c9fdccf3c8d7ccc9c7bc784130c0c0c25426cb
h-schroeder-coder <hankorschroeder@gmail.com>
2025-01-15 12:49:12 -0600

    Adding search engine function

--- src/App.js
+++ src/App.js
@@ -1,67 +1,23 @@
-import './App.css';
-import BookLover from './BookLover.png';
+import "./App.css";
+import BookLover from "./BookLover.png";
+import Footer from "./Footer";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={BookLover}
-          className="bookLoverImage img-fluid"
-          alt="bookLoverImage"
-        />
-        <h1>Dictionary</h1>
-      </header>
-      <footer className="App-footer">
-        This webpage was coded by{" "}
-        <a
-          href="https://github.com/h-schroeder-coder"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Hannah Schroeder,
-        </a>{" "}
-        a{" "}
-        <a
-          href="https://www.shecodes.io/graduates/157590-hannah-schroeder"
-          target="_blank"
-          rel="noreferrer"
-        >
-          graduate
-        </a>{" "}
-        of{" "}
-        <a
-          href="https://www.shecodes.io/workshops"
-          target="_blank"
-          rel="noreferrer"
-        >
-          SheCodes,
-        </a>{" "}
-        as part of the{" "}
-        <a
-          href="https://www.shecodes.io/workshops"
-          target="_blank"
-          rel="noreferrer"
-        >
-          SheCodes React Add-on Workshop,
-        </a>{" "}
-        is open source on{" "}
-        <a
-          href="https://github.com/h-schroeder-coder/shecodes_react_add-on_wk_1"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub,
-        </a>{" "}
-        and is hosted by{" "}
-        <a
-          href="https://h-schroeder-coder-dictionary.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netifly
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={BookLover}
+            className="bookLoverImage img-fluid"
+            alt="bookLoverImage"
+          />
+          <h1>Dictionary</h1>
+        </header>
+        <Dictionary />
+        <Footer />
+      </div>
     </div>
   );
 }
